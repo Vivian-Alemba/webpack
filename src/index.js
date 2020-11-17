a6cdecf6b7e67ba40285e85fc55daf98a8979b68
vivian-alemba <alembavivianauma@gmail.com>
2020-11-17 15:06:05 +0300

    files updated

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 import fooString from "./foo.js"
 import bar from "./bar.js"
-import "./item3.jpg"
+import "./style.css"
+import Icon from "./item3.jpg"
 
+
+
+
+function component(){
+    const element=document.createElement('div')
+    element.innerHTML=(['We operate worldwide'])
+    element.classList.add('hello')
+
+    //adding image to our existing div
+    const myIcon=new Image()
+    myIcon.src=Icon
+    element.appendChild(myIcon)
+    return element
+
+
+
+}
+
+document.body.appendChild(component())
 console.log(fooString,bar)
